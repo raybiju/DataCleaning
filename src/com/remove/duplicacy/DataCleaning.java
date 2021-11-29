95c4ec16125e56b84ff35217a72bd132952a8232
raybiju <bijay528yadav@gmail.com>
2021-11-29 15:03:28 +0530

    add Data cleaning

---
+++ src/com/remove/duplicacy/DataCleaning.java
@@ -0,0 +1,31 @@
+package com.remove.duplicacy;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+ class CSVFile {
+
+     public static void main(String[] args) throws Exception {
+
+         Scanner file = new Scanner(new File("C:\\Users\\Admin2\\Downloads\\Treadmill-social-media.csv"));
+         Set<String> wordSet = new HashSet<String>();
+         FileWriter data = new FileWriter("D:\\Data.txt"+System.currentTimeMillis());
+         file.useDelimiter(",");
+         String word;
+
+         while (file.hasNext())
+         {
+             word = file.next().replaceAll("[^a-zA-Z0-9 ]", "");
+             wordSet.add(word);
+
+
+         }
+         file.close();
+
+         for (String wordlist : wordSet) {
+             data.write(wordlist);
+         }
+     }
+ }
